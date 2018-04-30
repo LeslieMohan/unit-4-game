61805714559c7de38787e46679d934f049a203e0
Leslie Mohan <lesliemohan@Leslies-MacBook-Air.local>
2018-04-29 23:46:42 -0400

    add latest version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,104 @@
 $(document).ready(function() {
 
-var main = $("body");
-var buttons = main.find("#buttons");
+//set up variables
 
-// create the jewel button
+//select a random number to display at start of game between 19-120
+var randomNumber = Math.floor(Math.random()*101+19)
+//append randomnumber in html doc with div class="targetScore"
+$(".targetScore").text(randomNumber)
+console.log(randomNumber)
 
-//1. create for-loop to iterate through  array 
-for (var i=0; i<randomNumber.length; i++) {
-    //inside loop
-//2. create a variable named "jewelBtn" equal to $("<button>")
-var jewelBtn = $("<button>");
-//3. then give "jewelBtn" classes
-jewelBtn.addClass(".alphabet li")
-//4. give "jewelBtn" attributes
-jewelBtn.attr("data-letter" .this[i]);
-//5.give each "jewelBtn" an image
-jewelBtn.img(???????[i]);
-//6 append to #buttons div
-buttons.append(jewelBtn);
+//each button has to select a random number between 1-12(no appending to html stays hidden)
+var randomNumberBtn1 = Math.floor(Math.random()*11+1)
+var randomNumberBtn2 = Math.floor(Math.random()*11+1)
+var randomNumberBtn3= Math.floor(Math.random()*11+1)
+var randomNumberBtn4 = Math.floor(Math.random()*11+1)
+console.log(randomNumberBtn1,randomNumberBtn2,randomNumberBtn3,randomNumberBtn4,)
 
-  }
+var wins = 0;
+var loses = 0;
+var userScore = 0;
+
+
+
+//wins
+//function wins() {
+    // (userScore == randomNumber)
+    //$("#sentence").append("YOU WIN!").innerHTML
+
+
+//loses
+
+$(".userScoreCounter").text(userScore)
+
+$("#jewel1").on("click", ".gems", function() {
+    var userScore = userScore + randomNumberBtn1;
+    console.log("new userScore=" + userScore);
+    $(".userScoreCounter").text(userScore);
+    console.log(userScore)
+        //if win or lose situations
+        if (userScore == randomNumber){
+            winner();
+        }
+        else if (userScore > randomNumber) {
+            loser();
+        }
+
+})
+$("#jewel2").on("click",".gems", function() {
+    var userScore = userScore + randomNumberBtn2;
+    console.log("new userScore=" + userScore);
+    $(".userScoreCounter").text(userScore);
+        //if win or lose situations
+        if (userScore == randomNumber){
+            winner();
+        }
+        else if (userScore > randomNumber) {
+            loser();
+        }
+
+})
+$("#jewel3").on("click",".gems", function() {
+    var userScore = userScore + randomNumberBtn3;
+    console.log("new userScore=" + userScore);
+    $(".userScoreCounter").text(userScore);
+        //if win or lose situations
+        if (userScore == randomNumber){
+            winner();
+        }
+        else if (userScore > randomNumber) {
+            loser();
+        }
+
+})
+$("#jewel4").on("click", ".gems", function() {
+    var userScore = userScore + randomNumberBtn4;
+    console.log("new userScore=" + userScore);
+    $(".userScoreCounter").text(userScore);
+        //if win or lose situations
+        if (userScore == randomNumber){
+            winner();
+        }
+        else if (userScore > randomNumber) {
+            loser();
+        }
+
+});
+//reset the game when user either matches target score or goes over target score, append to html doc
+$(".wins").text(wins)
+$(".loses").text(loses)
+
+function reset() {
+
+    randomNumber=Math.floor(Math.random()*101+19);
+    $(".targetScore").text(randomNumber);
+    randomNumberBtn1 = Math.floor(Math.random()*11+1);
+    randomNumberBtn2 = Math.floor(Math.random()*11+1);
+    randomNumberBtn3= Math.floor(Math.random()*11+1);
+    randomNumberBtn4 = Math.floor(Math.random()*11+1);
+    userScore = 0;
+    $('#finalTotal').text(userScore);
 }
+
+
+});
